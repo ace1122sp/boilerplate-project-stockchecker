a6fb6216776e34dff07b35de890a44c798e98882
ace11 <ace11.space22@gmail.com>
2019-01-25 20:09:38 +0100

    patch

--- routes/api.js
+++ routes/api.js
@@ -8,16 +8,10 @@
 
 'use strict';
 
-var expect = require('chai').expect;
-var MongoClient = require('mongodb');
-
-const CONNECTION_STRING = process.env.DB; //MongoClient.connect(CONNECTION_STRING, function(err, db) {});
+const { getStock } = require('../controllers');
 
 module.exports = function (app) {
 
   app.route('/api/stock-prices')
-    .get(function (req, res){
-      
-    });
-    
+    .get(getStock);    
 };
