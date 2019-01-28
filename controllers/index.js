e8689ff01c75dd7fad0b6de23f4f683425aa7f08
ace11 <ace11.space22@gmail.com>
2019-01-28 17:34:12 +0100

    _fetchNewStock updated;  _findUpdatedStock method&tests

--- controllers/index.js
+++ controllers/index.js
@@ -1,8 +1,95 @@
-const getStock = (req, res) => {
-  // check out if there how many stocks are in query
-  
+const Stock = require('../models/stock');
+const { getLatestPrice, searchStock } = require('./api');
+
+const _oldPrice = date => {
+  const maxAge = 1000 * 60 * 60 * 24;
+  const activeDate = parseInt(date);
+  const now = Date.now();
+
+  const currentAge = now - activeDate;
+  const isOld = currentAge > maxAge ? true : false;
+  return isOld;
 }
 
+// _ to return promises
+// returns Stock instance which does not exist in db yet
+const _fetchNewStock = symbol => {
+  return searchStock(symbol)
+    .then(data => {
+      let stock = new Stock(data.data);
+      return stock        
+    })
+    .catch(err => {
+      // to handle
+    });
+}
+const _findUpdatedStock = (symbol, liked = false) => {    
+    return Stock.findOne({ symbol })
+      .then(stock => {
+        if (!stock) return _fetchNewStock(symbol)
+        return stock;
+      })
+      .then(stock => {        
+        if (_oldPrice(stock.date)) {
+          return getLatestPrice(stock.symbol)
+            .then(res => {
+              return stock.updatePrice(parseFloat(res.data.price))
+            })            
+            .catch(err => {
+              throw err;
+            });          
+        } else {
+          return stock;
+        }
+      })
+      .then(stock => {
+        if (liked) stock.increaseLikes();
+        return stock;
+      })
+      .then(stock => {        
+        return stock.save()
+      })
+      .catch(err => {
+        // to handle
+      });
+}
+
+const sanitizeAndValidateQueries = (req, res, next) => {
+  // additionally it formats stock queries in an array
+
+  // pseudo
+  let queries = [];
+  if (typeof req.query.stock === 'string') {
+    queries.push(req.query.stock);
+  } else {
+    queries = [req.query.stock[0], req.query.stock[1]];
+  }
+  req.query.stock = [...queries];
+  next();
+}
+
+const getStock = async (req, res, next) => {
+  if (!req.query.stock.length) next(); 
+  res.locals.stocks = [];
+  req.query.stock.forEach(symbol => {    
+    res.locals.stocks.push(_findUpdatedStock(symbol));
+  });
+  Promise.all(res.locals.stocks)
+    .then(stocks => {
+      res.json({ stockData: [...stocks] })
+    })
+    .catch(err => {
+      next(err);
+    });
+}
+
+const handleNoQueryStock = (req, res) => {}
+
 module.exports = {
-  getStock
+  _oldPrice,
+  _fetchNewStock,
+  _findUpdatedStock,
+  sanitizeAndValidateQueries,
+  getStock,
+  handleNoQueryStock
 }
--- controllers/index.test.js
+++ controllers/index.test.js
@@ -3,12 +3,29 @@
 require('dotenv').config(); // figure out if possible to use root .env
 
 const chai = require('chai');
-const { suite, test } = require('mocha');
+const mongoose = require('mongoose');
+const { suite, test, setup, suiteSetup, teardown, suiteTeardown } = require('mocha');
 const { _oldPrice, _fetchNewStock, _findUpdatedStock, sanitizeAndValidateQueries, getStock, handleNoQueryStock } = require('./index');
+const Stock = require('../models/stock');
 
 const assert = chai.assert;
 
 suite('controller functions', () => {
+  // test variables
+  const expectedKeys = ['name', 'price', 'symbol', 'date', 'currency', 'likes'];
+
+  // pre hook setup: connect to test-db before tests  
+  suiteSetup(async () => {
+    await mongoose.connect('mongodb://localhost:27017/stockpicker-test', { useNewUrlParser: true })
+      .then(() => {
+        console.log('connected to test db');
+      })
+      .catch(() => {
+        console.error(err.message);
+        process.exit(1);
+      })
+  });
+
   suite('_oldPrice()', () => {
     test('old date', done => {
       const oldDate = '1000';      
@@ -25,19 +42,69 @@ suite('controller functions', () => {
     });
   });
 
-  suite('_fetchNewStock(symbol)', () => {
-    test('valid usage');
+  suite('_fetchNewStock(symbol)', () => {    
+    const testSymbol = 'MSFT';
+    
+    test('valid usage', () => {      
+      _fetchNewStock(testSymbol)
+        .then(stock => {          
+          assert.hasAllKeys(stock, expectedKeys, 'some keys missing');
+          assert.propertyVal(stock, 'symbol', testSymbol);
+        })
+        .catch(() => {});
+    });
   });
 
-  suite('_findUpdatedStock(symbol, liked)', () => {
-    test('');
+  suite.only('_findUpdatedStock(symbol, liked)', () => {
+    // expected
+    const testSymbol = 'MSFT';
+
+    // test after all to delete added stocks
+    test('when stock not in db', () => {
+      _findUpdatedStock(testSymbol)
+        .then(stock => {
+          assert.hasAllKeys(stock, expectedKeys);
+          assert.strictEqual(stock.symbol, testSymbol);
+          assert.strictEqual(stock.likes, 0);
+          assert.strictEqual(stock.name, 'Microsoft Corporation');
+          assert.strictEqual(stock.currency, 'USD');
+        })
+        .catch(() => {});
+    });
+    test('when stock in db', () => {
+      // test variables
+      const timeBeforeTest = Date.now();
+
+      _findUpdatedStock(testSymbol)
+        .then(stock => {
+          assert.hasAllKeys(stock, expectedKeys);
+          assert.strictEqual(stock.symbol, testSymbol);
+          assert.strictEqual(stock.likes, 0);
+          assert.strictEqual(stock.name, 'Microsoft Corporation');
+          assert.strictEqual(stock.currency, 'USD');
+          assert.isBelow(parseInt(stock.date), parseInt(timeBeforeTest));
+        })
+        .catch(() => {});
+    });
+
+    test('when stock in db and arg liked=true', () => {
+      _findUpdatedStock(testSymbol, true)
+        .then(stock => {
+          assert.hasAllKeys(stock, expectedKeys);
+          assert.strictEqual(stock.symbol, testSymbol);
+          assert.strictEqual(stock.likes, 1);
+          assert.strictEqual(stock.name, 'Microsoft Corporation');
+          assert.strictEqual(stock.currency, 'USD');          
+        })
+        .catch(() => { });
+    });
   });
 
   suite('sanitizeAndValidateQueries()', () => {
     test('');
   });
 
-  suite('getStcok()', () => {
+  suite('getStock()', () => {
     test('');
   });
 
@@ -45,4 +112,8 @@ suite('controller functions', () => {
     test('');
   });
 
+  // suiteTeardown(() => {
+  //   console.log('tests finished');
+  //   process.exit(0);
+  // });
 });
