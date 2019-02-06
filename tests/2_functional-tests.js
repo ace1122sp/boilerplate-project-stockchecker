437791148dcf7de98acad2e9389af8e1407d83b8
ace11 <ace11.space22@gmail.com>
2019-02-06 17:34:07 +0100

    func tests

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -11,195 +11,225 @@ const chai = require('chai');
 const assert = chai.assert;
 const server = require('../server');
 const { suite, test, suiteTeardown } = require('mocha');
+const Voter = require('../models/voter');
+const Stock = require('../models/stock');
 
 chai.use(chaiHttp);
 
+const _dropCollection = Model => {
+  return Model.deleteMany({}, err => {
+    if (err) {
+      console.log(err); // to handle;
+    }
+  });
+}
+
+const teardownDB = done => 
+  Promise.all([_dropCollection(Voter), _dropCollection(Stock)])
+    .then(() => {
+      console.log('subsuite torn down ---');
+      done();
+    })
+    .catch(err => {}); // to handle
+
+// at this moment tests are passing only for first suite or any single suit under test
 suite('Functional Tests', function() {
     
     suite('GET /api/stock-prices => stockData object', function() {      
-      this.timeout(4999); 
+      this.timeout(19999); 
 
-      test('1 stock', function(done) {
-        // arrange
-        const expected = {
-          name: 'Alphabet Inc.',
-          symbol: 'GOOG',
-          price: undefined,
-          currency: 'USD',
-          likes: 0,
-          date: undefined
-        };
-        const expectedKeys = Object.keys(expected);
+      suite('1 stock tests suite', function() {
+        test('1 stock', function(done) {
+          // arrange
+          const expected = {
+            name: 'Alphabet Inc.',
+            symbol: 'GOOG',
+            price: undefined,
+            currency: 'USD',
+            likes: 0,
+            date: undefined
+          };
+          const expectedKeys = Object.keys(expected);
+  
+          // act 
+          chai.request(server)        
+            .get('/api/stock-prices')
+            .query({stock: 'goog'})
+            .end(function(err, res){
+              const actual = res.body.stockData[0];
+              
+              // assert
+              assert.hasAllKeys(actual, expectedKeys);
+              assert.strictEqual(actual.name, expected.name);
+              assert.strictEqual(actual.symbol, expected.symbol);
+              assert.strictEqual(actual.currency, expected.currency);
+              assert.strictEqual(actual.likes, expected.likes);
+  
+              done();
+          });
+        });
+        
+        test('1 stock with like', function(done) {
+          // arrange 
+          const expected = {
+            name: 'Alphabet Inc.',
+            symbol: 'GOOG',
+            price: undefined,
+            currency: 'USD',
+            likes: 1,
+            date: undefined
+          };
+          const expectedKeys = Object.keys(expected);
+          
+          // act 
+          chai.request(server)
+            .get('/api/stock-prices')
+            .query({ stock: 'goog', like: true })
+            .end(function(err, res) {
+              const actual = res.body.stockData[0];
+  
+              // assert
+              assert.hasAllKeys(actual, expectedKeys);
+              assert.strictEqual(actual.name, expected.name);
+              assert.strictEqual(actual.symbol, expected.symbol);
+              assert.strictEqual(actual.currency, expected.currency);
+              assert.strictEqual(actual.likes, expected.likes);
+  
+              done();
+            });
+        });
+        
+        test('1 stock with like again (ensure likes arent double counted)', function(done) {
+          // arrange
+          const expected = {
+            name: 'Alphabet Inc.',
+            symbol: 'GOOG',
+            price: undefined,
+            currency: 'USD',
+            likes: 1,
+            date: undefined
+          };
+          const expectedKeys = Object.keys(expected);
+  
+          // act
+          chai.request(server)
+            .get('/api/stock-prices')
+            .query({ stock: 'goog', like: true })
+            .end(function(err, res) {
+              const actual = res.body.stockData[0];
+              
+              // assert 
+              assert.hasAllKeys(actual, expectedKeys);
+              assert.strictEqual(actual.name, expected.name);
+              assert.strictEqual(actual.symbol, expected.symbol);
+              assert.strictEqual(actual.currency, expected.currency);
+              assert.strictEqual(actual.likes, expected.likes);
+  
+              done();
+            });
+        });
 
-        // act 
-        chai.request(server)        
-          .get('/api/stock-prices')
-          .query({stock: 'goog'})
-          .end(function(err, res){
-            const actual = res.body.stockData[0];
-            
-            // assert
-            assert.hasAllKeys(actual, expectedKeys);
-            assert.strictEqual(actual.name, expected.name);
-            assert.strictEqual(actual.symbol, expected.symbol);
-            assert.strictEqual(actual.currency, expected.currency);
-            assert.strictEqual(actual.likes, expected.likes);
-
-            done();
+        suiteTeardown(function(done) {
+          teardownDB(done);
         });
       });
-      
-      test('1 stock with like', function(done) {
-        // arrange 
-        const expected = {
-          name: 'Alphabet Inc.',
-          symbol: 'GOOG',
-          price: undefined,
-          currency: 'USD',
-          likes: 1,
-          date: undefined
-        };
-        const expectedKeys = Object.keys(expected);
+
+      suite('2 stocks tests suite', function() {
+        test('2 stocks', function(done) {
+          // arrange
+          const expected1 = {
+            name: 'Alphabet Inc.',
+            symbol: 'GOOG',
+            price: undefined,
+            currency: 'USD',
+            likes: 0,
+            date: undefined
+          };
+          const expected2 = {
+            name: 'Apple Inc.',
+            symbol: 'AAPL',
+            price: undefined,
+            currency: 'USD',
+            likes: 0,
+            date: undefined
+          };
+    
+          const expectedKeys = Object.keys(expected1);
+  
+          // act
+          chai.request(server)
+            .get('/api/stock-prices')
+            .query({ stock: ['goog', 'aapl'] })
+            .end(function(err, res) {
+              const actual1 = res.body.stockData[0];
+              const actual2 = res.body.stockData[1];
+  
+              // assert
+              assert.hasAllKeys(actual1, expectedKeys);
+              assert.strictEqual(actual1.name, expected1.name);
+              assert.strictEqual(actual1.symbol, expected1.symbol);
+              assert.strictEqual(actual1.currency, expected1.currency);
+              assert.strictEqual(actual1.likes, expected1.likes);
+  
+              assert.hasAllKeys(actual2, expectedKeys);
+              assert.strictEqual(actual2.name, expected2.name);
+              assert.strictEqual(actual2.symbol, expected2.symbol);
+              assert.strictEqual(actual2.currency, expected2.currency);
+              assert.strictEqual(actual2.likes, expected2.likes);
+  
+              done();
+            });
+        });
         
-        // act 
-        chai.request(server)
-          .get('/api/stock-prices')
-          .query({ stock: 'goog', like: true })
-          .end(function(err, res) {
-            const actual = res.body.stockData[0];
-
-            // assert
-            assert.hasAllKeys(actual, expectedKeys);
-            assert.strictEqual(actual.name, expected.name);
-            assert.strictEqual(actual.symbol, expected.symbol);
-            assert.strictEqual(actual.currency, expected.currency);
-            assert.strictEqual(actual.likes, expected.likes);
-
-            done();
-          });
-      });
-      
-      test('1 stock with like again (ensure likes arent double counted)', function(done) {
-        // arrange
-        const expected = {
-          name: 'Alphabet Inc.',
-          symbol: 'GOOG',
-          price: undefined,
-          currency: 'USD',
-          likes: 1,
-          date: undefined
-        };
-        const expectedKeys = Object.keys(expected);
-
-        // act
-        chai.request(server)
-          .get('/api/stock-prices')
-          .query({ stock: 'goog', like: true })
-          .end(function(err, res) {
-            const actual = res.body.stockData[0];
-            
-            // assert 
-            assert.hasAllKeys(actual, expectedKeys);
-            assert.strictEqual(actual.name, expected.name);
-            assert.strictEqual(actual.symbol, expected.symbol);
-            assert.strictEqual(actual.currency, expected.currency);
-            assert.strictEqual(actual.likes, expected.likes);
-
-            done();
-          });
-      });
-      
-      test('2 stocks', function(done) {
-        // arrange
-        const expected1 = {
-          name: 'Alphabet Inc.',
-          symbol: 'GOOG',
-          price: undefined,
-          currency: 'USD',
-          likes: 0,
-          date: undefined
-        };
-        const expected2 = {
-          name: 'Apple Inc.',
-          symbol: 'AAPL',
-          price: undefined,
-          currency: 'USD',
-          likes: 0,
-          date: undefined
-        };
+        test('2 stocks with like', function(done) {
+          // arrange
+          const expected1 = {
+            name: 'Alphabet Inc.',
+            symbol: 'GOOG',
+            price: undefined,
+            currency: 'USD',
+            likes: 1,
+            date: undefined
+          };
+          const expected2 = {
+            name: 'Apple Inc.',
+            symbol: 'AAPL',
+            price: undefined,
+            currency: 'USD',
+            likes: 1,
+            date: undefined
+          };
+    
+          const expectedKeys = Object.keys(expected1);
   
-        const expectedKeys = Object.keys(expected1);
-
-        // act
-        chai.request(server)
-          .get('/api/stock-prices')
-          .query({ stock: ['goog', 'aapl'] })
-          .end(function(err, res) {
-            const actual1 = res.body.stockData[0];
-            const actual2 = res.body.stockData[1];
-
-            // assert
-            assert.hasAllKeys(actual1, expectedKeys);
-            assert.strictEqual(actual1.name, expected1.name);
-            assert.strictEqual(actual1.symbol, expected1.symbol);
-            assert.strictEqual(actual1.currency, expected1.currency);
-            assert.strictEqual(actual1.likes, expected1.likes);
-
-            assert.hasAllKeys(actual2, expectedKeys);
-            assert.strictEqual(actual2.name, expected2.name);
-            assert.strictEqual(actual2.symbol, expected2.symbol);
-            assert.strictEqual(actual2.currency, expected2.currency);
-            assert.strictEqual(actual2.likes, expected2.likes);
-
-            done();
-          });
-      });
-      
-      test('2 stocks with like', function(done) {
-        // arrange
-        const expected1 = {
-          name: 'Alphabet Inc.',
-          symbol: 'GOOG',
-          price: undefined,
-          currency: 'USD',
-          likes: 1,
-          date: undefined
-        };
-        const expected2 = {
-          name: 'Apple Inc.',
-          symbol: 'AAPL',
-          price: undefined,
-          currency: 'USD',
-          likes: 1,
-          date: undefined
-        };
+          // act
+          chai.request(server)
+            .get('/api/stock-prices')
+            .query({ stock: ['goog', 'aapl'], like: 1 })
+            .end(function(err, res) {
+              const actual1 = res.body.stockData[0];
+              const actual2 = res.body.stockData[1];
   
-        const expectedKeys = Object.keys(expected1);
+              // assert
+              assert.hasAllKeys(actual1, expectedKeys);
+              assert.strictEqual(actual1.name, expected1.name);
+              assert.strictEqual(actual1.symbol, expected1.symbol);
+              assert.strictEqual(actual1.currency, expected1.currency);
+              assert.strictEqual(actual1.likes, expected1.likes);
+  
+              assert.hasAllKeys(actual2, expectedKeys);
+              assert.strictEqual(actual2.name, expected2.name);
+              assert.strictEqual(actual2.symbol, expected2.symbol);
+              assert.strictEqual(actual2.currency, expected2.currency);
+              assert.strictEqual(actual2.likes, expected2.likes);
+  
+              done();
+            });
+        });
 
-        // act
-        chai.request(server)
-          .get('/api/stock-prices')
-          .query({ stock: ['goog', 'aapl'], like: 1 })
-          .end(function(err, res) {
-            const actual1 = res.body.stockData[0];
-            const actual2 = res.body.stockData[1];
-
-            // assert
-            assert.hasAllKeys(actual1, expectedKeys);
-            assert.strictEqual(actual1.name, expected1.name);
-            assert.strictEqual(actual1.symbol, expected1.symbol);
-            assert.strictEqual(actual1.currency, expected1.currency);
-            assert.strictEqual(actual1.likes, expected1.likes);
-
-            assert.hasAllKeys(actual2, expectedKeys);
-            assert.strictEqual(actual2.name, expected2.name);
-            assert.strictEqual(actual2.symbol, expected2.symbol);
-            assert.strictEqual(actual2.currency, expected2.currency);
-            assert.strictEqual(actual2.likes, expected2.likes);
-
-            done();
-          });
-      });
-      
+        suiteTeardown(function(done) {
+          teardownDB(done);
+        });
+      });    
     });
 });
