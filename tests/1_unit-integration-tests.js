8169f00553ce7b6d4a8fd013dc8ea90cf2e034cc
ace11 <ace11.space22@gmail.com>
2019-02-07 16:41:26 +0100

    unit-integration-tests: using only devErrorHandler

--- tests/1_unit-integration-tests.js
+++ tests/1_unit-integration-tests.js
@@ -8,12 +8,11 @@
 
 const mongoose = require('mongoose');
 const { suite, suiteSetup, suiteTeardown } = require('mocha');
-const config = require('../config');
-const errorHandler = config.app.env === 'PRODUCTION' ? require('../libs/prodErrorHandler') : require('../libs/devErrorHandler');
+const errorHandler = require('../libs/devErrorHandler');
 const apiUnitTests = require('../controllers/api.test');
 const controllersUnitTests = require('../controllers/index.test');
 
-suite.skip('Unit Tests', function(){
+suite('Unit Tests', function(){
   suiteSetup(async () => {
     await mongoose.connect('mongodb://localhost:27017/stockpicker-test', { useNewUrlParser: true })
       .then(() => {
