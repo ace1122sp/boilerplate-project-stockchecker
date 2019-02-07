2239d8b10487e9f59a9853b6780e526abc187908
ace11 <ace11.space22@gmail.com>
2019-02-07 17:57:59 +0100

    added error handlers modules

--- libs/devErrorHandler.js
+++ libs/devErrorHandler.js
@@ -1,15 +1,34 @@
 module.exports = (() => {
-  const handle = err => {
+  const _handle = err => {
     console.log(error);
     process.exit(1);
   };
 
   const handleTest = err => {
     console.log(err);
-  }
+  };
+
+  const handleApi = err => {
+    console.error(err);
+    return { message: err.message, status: 'error' };
+  };
+
+  const logErrors = (err, req, res, next) => {
+    console.error(err);
+    next(err);
+  };
+
+  const handleClientResponse = (err, req, res, next) => {
+    res.sendStatus(500);      
+  };
+
+  const handleVoters = _handle;
 
   return {
-    handle, 
-    handleTest
+    handleTest,
+    handleApi,
+    handleVoters,
+    logErrors, 
+    handleClientResponse 
   }
 })();
--- libs/prodErrorHandler.js
+++ libs/prodErrorHandler.js
@@ -1,14 +1,32 @@
 module.exports = (() => {
-  const handle = err => {
-    // to figure out 
+  const _handle = err => {
+    throw err;
   }
 
   const handleTest = err => {
     console.log('Error ---');
+  };
+
+  const handleApi = err => {
+    return { message: 'server error', status: 'error' };
+  };
+
+  const handleVoters = _handle;
+
+  const logErrors = (err, req, res, next) => {
+    console.log('error: ', err.message);
+    next(err);
+  };
+
+  const handleClientResponse = (err, req, res, next) => {
+    res.sendStatus(500);
   }
 
   return {
-    handle, 
-    handleTest
+    handleTest,
+    handleApi,
+    handleVoters,
+    logErrors, 
+    handleClientResponse
   }
 })();
