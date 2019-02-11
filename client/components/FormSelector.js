c42148b80893cfdaea679051dcc8050e4ff72923
ace11 <ace11.space22@gmail.com>
2019-02-11 18:09:09 +0100

    added FormSelector component

---
+++ client/components/FormSelector.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const FormSelector = () => {
+  return (
+    <div>
+      <button>find your stock</button>
+      <button>compare two stocks</button>
+    </div>
+  );
+}
+
+export default FormSelector;
