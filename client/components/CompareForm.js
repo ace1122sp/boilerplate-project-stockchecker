d85117b2933f4d421d6b3c32103f3934c729db9e
ace11 <ace11.space22@gmail.com>
2019-02-13 15:37:07 +0100

    CompareForm wired

--- client/components/CompareForm.js
+++ client/components/CompareForm.js
@@ -1,17 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { apiUrl } from '../urls';
+import { STOCK_PANEL, NOT_FOUND_PANEL } from './constants';
 
-const CompareForm = ({ changeActiveComponent }) => {
+
+const CompareForm = ({ changeActiveComponent, updateStocks }) => {
+  const [loading, setLoadingStatus] = useState(false);
+
   const cancel = () => {
     changeActiveComponent();
   };
 
+  const handleRequest = e => {
+    e.preventDefault();
+    setLoadingStatus(true);    
+    const unsafeStocks = [e.target.stockA.value, e.target.stockB.value];
+    const like = e.target.like.checked;
+    let url = apiUrl + `stock=${unsafeStocks[0]}&stock=${unsafeStocks[1]}`;
+
+    if (like) url +=`&like=${like}`;
+
+    fetch(url) 
+      .then(res => {
+        if (!res) return null;
+        return res.json();
+      })
+      .then(res => {
+        setLoadingStatus(false);
+        updateStocks([...res.stockData]);
+
+        const panelToRender = res ? STOCK_PANEL : NOT_FOUND_PANEL;
+        changeActiveComponent(panelToRender);
+      })
+      .catch(err => {}); // to handle
+  }
+
+  const loadingSpan = <span>loading...</span>
+
   return (
     <div>
+      {loading && loadingSpan}
       <button onClick={cancel}>x</button>
-      <form>
-        <input type='text' placeholder='stock a' autoFocus required name='stock-a' id='stock-input-a' />
-        <input type='text' placeholder='stock b' required name='stock-b' id='stock-input-b' />
-        <input type='checkbox' id='like-input' />
+      <form onSubmit={handleRequest}>
+        <input type='text' placeholder='stock a' autoFocus required name='stockA' id='stock-input-a' />
+        <input type='text' placeholder='stock b' required name='stockB' id='stock-input-b' />
+        <input type='checkbox' name='like' id='like-input' />
         <label htmlFor='like-input'>like both</label>
         <br />
         <button>compare</button>
