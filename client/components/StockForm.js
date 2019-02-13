53ec06674dec7d38e850ed57813fd446e8c700f6
ace11 <ace11.space22@gmail.com>
2019-02-13 15:17:59 +0100

    StockForm wired

--- client/components/StockForm.js
+++ client/components/StockForm.js
@@ -1,16 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { apiUrl } from '../urls';
+import { STOCK_PANEL, NOT_FOUND_PANEL } from './constants';
 
-const StockForm = ({ changeActiveComponent }) => {
+const StockForm = ({ changeActiveComponent, updateStocks }) => {
+  const [loading, setLoadingStatus] = useState(false);
+
   const cancel = () => {
     changeActiveComponent();
   };
 
+  const handleRequest = e => {
+    e.preventDefault();
+    setLoadingStatus(true);
+    const unsafeStock = e.target.stock.value;
+    const like = e.target.like.checked;
+    let url = apiUrl + `stock=${unsafeStock}`;
+
+    if (like) url += `&like=${like}`;
+    fetch(url)
+      .then(res => {
+        if (!res) return null;
+        return res.json();
+      })
+      .then(res => {
+        setLoadingStatus(false);        
+        updateStocks([res.stockData[0]]);
+        if (res) {
+          changeActiveComponent(STOCK_PANEL);          
+        } else {
+          changeActiveComponent(NOT_FOUND_PANEL);
+        }
+      })
+      .catch(err => {}) // to handle
+  };
+
+  const loadingSpan = <span>loading...</span>;
+
   return (
     <div>
+      {loading && loadingSpan}
       <button onClick={cancel}>x</button>
-      <form>
+      <form onSubmit={handleRequest}>
         <input type='text' placeholder='stock' autoFocus required name='stock' id='stock-input' />
-        <input type='checkbox' id='like-input'/>
+        <input type='checkbox' name='like' id='like-input'/>
         <label htmlFor='like-input'>like</label>
         <button>get price</button>
       </form>
