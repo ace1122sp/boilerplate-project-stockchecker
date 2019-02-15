bbee9dbcf9d1ef2605b02b7bcdbb01a74ac8dfed
ace11 <ace11.space22@gmail.com>
2019-02-15 19:39:38 +0100

    css: basic style for ErrorPanel component

--- client/components/ErrorPanel.js
+++ client/components/ErrorPanel.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+import '../css/NotFoundPanel.scss';
 
 const ErrorPanel = ({ changeActiveComponent }) => {
   const cancel = () => {
@@ -6,9 +9,10 @@ const ErrorPanel = ({ changeActiveComponent }) => {
   }
 
   return (
-    <div>
-      <button onClick={cancel}>x</button>
+    <div className='wrapper-with-close'>
+      <button onClick={cancel} className='close-btn'>x</button>
       <h2>Snap... Something went wrong!</h2>
+      <FontAwesomeIcon icon='eye-slash' size='2x' />  
     </div>
   );
 }
