f0d5d0ea0d4debb5455d7abde7a576c6e68b7fbe
ace11 <ace11.space22@gmail.com>
2019-02-15 12:57:50 +0100

    StockPanel: component calculates & shows relative likes

--- client/components/StockPanel.js
+++ client/components/StockPanel.js
@@ -6,7 +6,7 @@ const Panel = ({ stock }) =>
     <p>{stock.name}</p>
     <p>{stock.currency} {stock.price}</p>
     <p>{stock.likes}</p>
-    <label>likes</label>
+    {typeof stock.likes === 'number' && <label>likes</label>}
   </div>
 
 const StockPanel = ({ stocks, changeActiveComponent }) => {
@@ -14,7 +14,16 @@ const StockPanel = ({ stocks, changeActiveComponent }) => {
     changeActiveComponent();
   }
 
-  const panels = stocks.map((stock, i) => <Panel key={i} stock={stock} />);
+  const modifiedStocks = [...stocks];
+  const shouldLikesBeRelative =  modifiedStocks.length === 2 && typeof modifiedStocks[1].likes === 'number';
+
+  if (shouldLikesBeRelative) {
+    modifiedStocks[0].likes = modifiedStocks[0].likes - modifiedStocks[1].likes;
+    modifiedStocks[1].likes = (-1) * modifiedStocks[0].likes;
+  }
+
+  const panels = modifiedStocks.map((stock, i) => <Panel key={i} stock={stock} />);
+
   return (
     <div>
       <button onClick={cancel}>x</button>
