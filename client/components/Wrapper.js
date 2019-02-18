e056b2cc63b5a6b84611c1765d6f62266a47f9a4
ace11 <ace11.space22@gmail.com>
2019-02-18 18:10:36 +0100

    added brand icons; css updates

--- client/components/Wrapper.js
+++ client/components/Wrapper.js
@@ -1,21 +1,24 @@
 import React from 'react';
 import Main from './Main';
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faChartLine, faBalanceScale, faThumbsUp, faEyeSlash, faSpinner, faHouseDamage  } from '@fortawesome/free-solid-svg-icons'
+import Footer from './Footer';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { fab } from '@fortawesome/free-brands-svg-icons'
+import { faChartLine, faBalanceScale, faThumbsUp, faEyeSlash, faSpinner, faHouseDamage, faFolder } from '@fortawesome/free-solid-svg-icons';
 
-library.add(faChartLine, faBalanceScale, faThumbsUp, faEyeSlash, faSpinner, faHouseDamage );
+library.add(fab, faChartLine, faBalanceScale, faThumbsUp, faEyeSlash, faSpinner, faHouseDamage, faFolder );
 
 import '../css/Wrapper.scss';
 
 const Wrapper = () => 
     <div>
       <header>
-        <h1>Stockçhecker</h1>
+        <h1>Stockchecker</h1>
       </header>
       <main>
         <Main />
       </main>
+      <Footer />
     </div>
 
 export default Wrapper;
