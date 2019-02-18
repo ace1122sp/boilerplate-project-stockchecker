ecb9ebdaa7f71bd7e9ede6569ecec3debb706002
ace11 <ace11.space22@gmail.com>
2019-02-18 18:09:22 +0100

    added Footer component and css for it

---
+++ client/components/Footer.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+import '../css/Footer.scss';
+
+const Footer = () => {
+  return (
+    <footer className='footer'>
+      <address>created by ace11</address>
+      <ul className='social-media'>
+        <li><a href='https://github.com/ace1122sp' target='_blank'><FontAwesomeIcon icon={['fab', 'github']} /> github | </a></li>
+        <li><a href='https://www.linkedin.com/in/aleksandar-bulovi%C4%87-83aa74139/' target='_blank'><FontAwesomeIcon icon={['fab', 'linkedin']} /> linkedin | </a></li>
+        <li><a href='https://ace1122sp.github.io/portfolio/' target='_blank'><FontAwesomeIcon icon='folder' /> portfolio </a></li>
+      </ul>
+    </footer>
+  );
+}
+
+export default Footer;
