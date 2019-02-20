ccf6e82aa929b37c97db158ff54c98fc869f1511
ace11 <ace11.space22@gmail.com>
2019-02-20 17:19:33 +0100

    Error.js: changed message

--- client/components/ErrorPanel.js
+++ client/components/ErrorPanel.js
@@ -11,7 +11,7 @@ const ErrorPanel = ({ changeActiveComponent }) => {
   return (
     <div className='wrapper-with-close'>
       <button onClick={cancel} className='close-btn'>x</button>
-      <h2>Snap... Something went wrong!</h2>
+      <h2>Snap... We've made too many requests to our free API!</h2>
       <FontAwesomeIcon icon='house-damage' size='2x' />  
     </div>
   );
